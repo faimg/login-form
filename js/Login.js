3219178db0121f21c9c5d419475a778adaaf7f0b
Edy Segura <edysegura@gmail.com>
2015-03-21 14:25:46 -0300

    Update Login.js

--- js/Login.js
+++ js/Login.js
@@ -1,7 +1,26 @@
 var Login = {
 
 	init: function() {
-		alert('Hi there!');
+		Login.setForm();
+	},
+
+	setForm: function() {
+		var form = document.getElementById('frmLogin');
+		if(form) {
+			
+			var ouvinteRaul = function(event){
+				console.log('Hey Raul, the form was submitted!');
+				event.preventDefault();
+			};
+
+			var ouvinteNatalia = function(event) {
+				console.log('Hi Natália! The form was submitted!');
+				event.preventDefault();
+			};
+
+			form.addEventListener('submit', ouvinteRaul);
+			form.addEventListener('submit', ouvinteNatalia);
+		}		
 	}
 
 };
